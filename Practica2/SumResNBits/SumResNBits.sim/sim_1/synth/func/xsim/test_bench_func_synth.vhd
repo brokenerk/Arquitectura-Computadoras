5029e4d40776f5483df32714ee2ffd2f5ee3bf05
Enrike Ramos <39656338+brokenerk@users.noreply.github.com>
2020-02-22 16:25:23 -0600

    reportes p2 y p3

--- Practica2/SumResNBits/SumResNBits.sim/sim_1/synth/func/xsim/test_bench_func_synth.vhd
+++ Practica2/SumResNBits/SumResNBits.sim/sim_1/synth/func/xsim/test_bench_func_synth.vhd
@@ -1,7 +1,7 @@
 -- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 -- --------------------------------------------------------------------------------
 -- Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
--- Date        : Sat Feb 22 15:40:45 2020
+-- Date        : Sat Feb 22 15:52:42 2020
 -- Host        : OUTERHEAVEN running 64-bit major release  (build 9200)
 -- Command     : write_vhdl -mode funcsim -nolib -force -file {C:/Users/YaKerTaker/Google
 --               Drive/8vo/Arquitectura-Computadoras/Practica2/SumResNBits/SumResNBits.sim/sim_1/synth/func/xsim/test_bench_func_synth.vhd}
