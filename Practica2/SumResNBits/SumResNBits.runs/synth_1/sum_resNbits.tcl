ddc97b7162bf97af0a68cc8ddf68333eb11c2b33
Enrike Ramos <39656338+brokenerk@users.noreply.github.com>
2020-02-27 19:43:53 -0600

    asginacion pines

--- Practica2/SumResNBits/SumResNBits.runs/synth_1/sum_resNbits.tcl
+++ Practica2/SumResNBits/SumResNBits.runs/synth_1/sum_resNbits.tcl
@@ -17,8 +17,6 @@ proc create_report { reportName command } {
     send_msg_id runtcl-5 warning "$msg"
   }
 }
-set_msg_config -id {Synth 8-256} -limit 10000
-set_msg_config -id {Synth 8-638} -limit 10000
 create_project -in_memory -part xc7a100tcsg324-1
 
 set_param project.singleFileAddWarning.threshold 0
@@ -42,6 +40,9 @@ read_vhdl -library xil_defaultlib {
 foreach dcp [get_files -quiet -all -filter file_type=="Design\ Checkpoint"] {
   set_property used_in_implementation false $dcp
 }
+read_xdc {{C:/Users/YaKerTaker/Google Drive/8vo/Arquitectura-Computadoras/Practica2/SumResNBits/SumResNBits.srcs/constrs_1/new/pines.xdc}}
+set_property used_in_implementation false [get_files {{C:/Users/YaKerTaker/Google Drive/8vo/Arquitectura-Computadoras/Practica2/SumResNBits/SumResNBits.srcs/constrs_1/new/pines.xdc}}]
+
 set_param ips.enableIPCacheLiteLoad 1
 close [open __synthesis_is_running__ w]
 
--- Practica3/SumAcarrAnt/SumAcarrAnt.runs/synth_1/sum_AnticNbits.tcl
+++ Practica3/SumAcarrAnt/SumAcarrAnt.runs/synth_1/sum_AnticNbits.tcl
@@ -17,6 +17,7 @@ proc create_report { reportName command } {
     send_msg_id runtcl-5 warning "$msg"
   }
 }
+set_param chipscope.maxJobs 1
 create_project -in_memory -part xc7a100tcsg324-1
 
 set_param project.singleFileAddWarning.threshold 0
@@ -37,6 +38,9 @@ read_vhdl -library xil_defaultlib {{C:/Users/YaKerTaker/Google Drive/8vo/Arquite
 foreach dcp [get_files -quiet -all -filter file_type=="Design\ Checkpoint"] {
   set_property used_in_implementation false $dcp
 }
+read_xdc {{C:/Users/YaKerTaker/Google Drive/8vo/Arquitectura-Computadoras/Practica3/SumAcarrAnt/SumAcarrAnt.srcs/constrs_1/new/pines.xdc}}
+set_property used_in_implementation false [get_files {{C:/Users/YaKerTaker/Google Drive/8vo/Arquitectura-Computadoras/Practica3/SumAcarrAnt/SumAcarrAnt.srcs/constrs_1/new/pines.xdc}}]
+
 set_param ips.enableIPCacheLiteLoad 1
 close [open __synthesis_is_running__ w]
 
